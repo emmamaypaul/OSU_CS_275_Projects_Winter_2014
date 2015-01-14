7021a72487cf6e2719dba8d574537f2fd340ddbd
emmamaypaul <emmamaypaul@gmail.com>
2015-01-14 15:10:48 -0800

    Update timberline.php

--- Final_Project/timberline.php
+++ Final_Project/timberline.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('display_errors', 'On'); //error report
-$mysqli = new mysqli("oniddb.cws.oregonstate.edu","pauli-db","ShhOnXOEe6IVSHUB","pauli-db"); //Connects to the database
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu","pauli-db","XXXXXXXXXXX","pauli-db"); //Connects to the database
 if($mysqli->connect_errno)
 {
 	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
